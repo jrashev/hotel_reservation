f17f0386b762a9e1a5a1467a554e8c35143b8922
jrashev <jrashev@gmail.com>
2024-03-02 21:44:56 +0200

    test-que

--- solution.js
+++ solution.js
@@ -22,20 +22,21 @@ function cleanData(e) {
     changeContent('search-form-content');
 }
 
+changeContent('search-form-content');
+document.querySelector('#search-form-button').addEventListener('click', (e) => searchFormData(e));
 
-
-document.querySelector('#confirm-back-btn').addEventListener('click', (e) => getBackToPersonalData(e));
-
-function getBackToPersonalData(e) {
+function searchFormData(e) {
     e.preventDefault();
-    changeContent('guest-details-form-content');
-}
-
-
-document.querySelector('#confirm-reservation').addEventListener('click', (e) => showThanksPage(e));
-
-function showThanksPage(e) {
-    e.preventDefault();
-    changeContent('thank-you-content');
-
+    const data = e.target.parentElement;
+    const checkIn = data.querySelector('#check-in').value;
+    const checkOut = data.querySelector('#check-out').value;
+    const people = data.querySelector('#people').value;
+    if (checkIn != '' && checkOut != '' && people != '' &&
+        new Date(checkIn) <= new Date(checkOut)) {
+        reservation.startDate = checkIn;
+        reservation.endDate = checkOut;
+        reservation.guestsCount = people;
+        console.log(reservation);
+        changeContent('search-result-form-content');
+    }
 }
