286cc28db69d02824097060773e6f48ea1c71d4d
jrashev <jrashev@gmail.com>
2024-03-02 21:43:50 +0200

    test-ver

--- solution.js
+++ solution.js
@@ -23,7 +23,6 @@ function cleanData(e) {
 }
 
 
-
 document.querySelector('#confirm-back-btn').addEventListener('click', (e) => getBackToPersonalData(e));
 
 function getBackToPersonalData(e) {
@@ -37,5 +36,4 @@ document.querySelector('#confirm-reservation').addEventListener('click', (e) => 
 function showThanksPage(e) {
     e.preventDefault();
     changeContent('thank-you-content');
-
 }
